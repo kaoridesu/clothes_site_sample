f125ff035dc507fdaa7dbc2d605b32b6e2bd28bc
KaoriTakashimizu <>
2021-06-05 21:40:39 +0900

    no message

---
+++ clothes_site_sample/clothes_site_sample/scripts/Master_clothose.cs
@@ -0,0 +1,28 @@
+namespace clothes_site_sample.scripts
+{
+    public class Master_clothose
+    {
+        public int id;
+        public string kind;
+        public string product_name;
+        public string size;
+        public string color;
+        public int price;
+        public string material;
+        public string product_introduction;
+       
+        public Master_clothose(int id, string kind, string productName, string size, string color, int price, string material, string productIntroduction)
+        {
+            this.id = id;
+            this.kind = kind;
+            product_name = productName;
+            this.size = size;
+            this.color = color;
+            this.price = price;
+            this.material = material;
+            product_introduction = productIntroduction;
+        }
+
+        
+    }
+}
