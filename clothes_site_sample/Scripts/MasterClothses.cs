01a4666507e62648a3d9216033dbba35a303a4d9
hidenami-i <shuhei.iwamoto.work@gmail.com>
2021-06-13 12:25:41 +0900

    エラー出てたところを修正

--- clothes_site_sample/Scripts/MasterClothses.cs
+++ clothes_site_sample/Scripts/MasterClothses.cs
@@ -1,28 +1,24 @@
 namespace clothes_site_sample.scripts
 {
-    public class Master_clothses
+    public class MasterCloth
     {
         public int id;
         public string kind;
         public string product_name;
-        public string size;
         public string color;
         public int price;
         public string material;
         public string product_introduction;
-       
-        public Master_clothses(int id, string kind, string productName, string size, string color, int price, string material, string productIntroduction)
+
+        public MasterCloth(int id, string kind, string productName, int price,
+            string material, string productIntroduction)
         {
             this.id = id;
             this.kind = kind;
             product_name = productName;
-            this.size = size;
-            this.color = color;
             this.price = price;
             this.material = material;
             product_introduction = productIntroduction;
         }
-
-        
     }
 }
