55e12099ff8da4c27c032820a336f1250af284cd
hidenami-i <shuhei.iwamoto.work@gmail.com>
2021-06-25 20:59:29 +0900

    fix

--- clothes_site_sample/Scripts/Tables/MasterPickupRepository.cs
+++ clothes_site_sample/Scripts/Tables/MasterPickupRepository.cs
@@ -1,7 +1,9 @@
+using clothes_site_sample.scripts.Bases;
+
 namespace clothes_site_sample.Scripts.Tables
 {
-    public class MasterPickupRepository
+    public class MasterPickupRepository : RepositoryBase<MasterPickupEntity, MasterPickupRepository>
+
     {
-        
     }
 }
