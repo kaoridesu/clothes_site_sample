b229fa3c1046bdcd7d304b4723f26a824065c77a
KaoriTakashimizu <kaoqoo1206@gmail.com>
2021-06-28 17:32:24 +0900

    no message

--- clothes_site_sample/Scripts/Tables/MasterClothImageEntity.cs
+++ clothes_site_sample/Scripts/Tables/MasterClothImageEntity.cs
@@ -6,7 +6,7 @@ namespace clothes_site_sample.Scripts.Tables
     public class MasterClothImageEntity : EntityBase
     {
         [JsonPropertyName("id")] [JsonInclude] public int Id { get; private set; }
-        [JsonPropertyName("master_clothes_id")] [JsonInclude] public int MasterClothId { get; private set; }
+        [JsonPropertyName("master_clothes_id")] [JsonInclude] public bool MasterClothId { get; private set; }
         [JsonPropertyName("url")] [JsonInclude] public string Url { get; private set; }
         [JsonPropertyName("is_main")] [JsonInclude] public bool IsMain { get; private set; }
         [JsonPropertyName("is_sub")] [JsonInclude] public bool IsSub { get; private set; }
