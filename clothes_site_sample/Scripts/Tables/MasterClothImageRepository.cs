5d12e7d1c2ec8d91fdf677efa9ba359c14063fc1
KaoriTakashimizu <kaoqoo1206@gmail.com>
2021-07-02 00:49:14 +0900

    no message

--- clothes_site_sample/Scripts/Tables/MasterClothImageRepository.cs
+++ clothes_site_sample/Scripts/Tables/MasterClothImageRepository.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
 using clothes_site_sample.scripts.Bases;
-
 namespace clothes_site_sample.Scripts.Tables
 {
     public class MasterClothImageRepository : RepositoryBase<MasterClothImageEntity, MasterClothImageRepository>
     {
+        public List<MasterClothImageEntity> FindAllById(int id)
+        {
+            return FindAllBy(x => x.Id == id);
+        }
     }
 }
